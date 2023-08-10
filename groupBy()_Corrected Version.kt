5f7e1877c2d7c5f1e4becf5b75282812b9bc2438
SilDev <115029411+ELIJAHMAITHYA@users.noreply.github.com>
2023-08-10 11:01:22 +0300

    Add files via upload

---
+++ groupBy()_Corrected Version.kt
@@ -0,0 +1,68 @@
+class Cookie(
+    val name: String,
+    val softBaked: Boolean,
+    val hasFilling: Boolean,
+    var price: Double
+)
+
+val cookies = listOf(
+    Cookie(
+        name = "Chocolate Chip",
+        softBaked = false,
+        hasFilling = false,
+        price = 1.69
+    ),
+    Cookie(
+        name = "Banana Walnut",
+        softBaked = true,
+        hasFilling = false,
+        price = 1.49
+    ),
+    Cookie(
+        name = "Vanilla Creme",
+        softBaked = false,
+        hasFilling = true,
+        price = 1.59
+    ),
+    Cookie(
+        name = "Chocolate Peanut Butter",
+        softBaked = false,
+        hasFilling = true,
+        price = 1.49
+    ),
+    Cookie(
+        name = "Snickerdoodle",
+        softBaked = true,
+        hasFilling = false,
+        price = 1.39
+    ),
+    Cookie(
+        name = "Blueberry Tart",
+        softBaked = true,
+        hasFilling = true,
+        price = 1.79
+    ),
+    Cookie(
+        name = "Sugar and Sprinkles",
+        softBaked = false,
+        hasFilling = false,
+        price = 1.39
+    )
+)
+val groupcookies = cookies.groupBy{
+    it.hasFilling
+}
+
+fun main() {
+    val hasfill= groupcookies[true] ?: listOf()
+    val nofill= groupcookies[false] ?: listOf()
+    hasfill.forEach{
+        println("${it.name} : ${it.price}")
+                }
+    println()
+    nofill.forEach{
+     println("${it.name} - ${it.price * 100}")   
+    }
+ 
+    
+                }
